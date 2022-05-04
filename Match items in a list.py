207feb84a3f605d6ef4f7515a2b6f7ba240be32e
DanielFit <104741488+DanielFit@users.noreply.github.com>
2022-05-04 15:29:05 +1000

    Update Match items in a list.py

--- Match items in a list.py
+++ Match items in a list.py
@@ -1,11 +1,13 @@
-
+"""The purpose of this code is to take a list of int and see if which two int's will equal a certain target int"""
 
 
 
 def twosum(nums: list[int], target: int) -> list[int]:
+        # As a means to simplify the soloution, if there are only 2 items in a list in this case, they will have to be the answer"
         if len(nums) == 2:
             return [0, 1]
 
+#The logic I am using is to start by subtracting one of the int from the list from the target, then check if that sum will equal another value in the list
         for index in range(0, len(nums) - 1):
             try:
                 x = target - nums[index]
